95baa36f890d9a10e8b36c7a7c1caee35050b5f2
Matthieu Duclos <maduclos@z1r12p2.le-101.fr>
2018-08-19 14:30:13 +0200

    failure : inter

--- t2/2-0/inter/inter.c
+++ t2/2-0/inter/inter.c
@@ -5,14 +5,36 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
+void ft_inter(char *str1, char *str2)
 {
+    if (str1[0] == '\0' || str2[0] == '\0')
+        return ;
+
     int i;
+    int j;
 
     i = 0;
-    while (str[i])
+    j = 0;
+    while (str1[j])
     {
-        ft_putchar(str[i]);
-        i++;
+        while (str2[i])
+        {
+            if (str2[i] == str1[j])
+            {
+                ft_putchar(str1[j]);
+                break ;
+            }
+            i++;
+        }
+        i = 0;
+        j++;
     }
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 3)
+        ft_inter(av[1], av[2]);
+    ft_putchar('\n');
+    return (0);
 }
